f21390c76cc5a41b9ad36610a4dd0d5f0422e3e7
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-10 08:46:15 -0800

    Set header as my name with class named header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,12 @@
 import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>Rudy Menjivar</h1>
+      </div>
+    )
+  }
+}
+  
