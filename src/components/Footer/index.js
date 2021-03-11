bea553a3bd5532ae8f74129b5b4c22b99ff0f722
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-11 14:41:28 -0800

    Added rel=noreferrer to links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,10 +5,10 @@ export default class Footer extends Component {
     return (
       <div className="fixed-bottom text-center">
         <nav className="navbar-dark bg-dark">
-          <a className="navbar-brand" href="https://github.com/Rudy-Menjivar" target="_blank">
+          <a className="navbar-brand" href="https://github.com/Rudy-Menjivar" target="_blank" rel="noreferrer">
             GitHub
           </a>
-          <a className="navbar-brand" href="https://www.linkedin.com/in/rudy-menjivar-" target="_blank">
+          <a className="navbar-brand" href="https://www.linkedin.com/in/rudy-menjivar-" target="_blank" rel="noreferrer">
             LinkedIn
           </a>
         </nav>
