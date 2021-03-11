da4e22cd74e643a81a902c94d9ad659b704c79db
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-10 17:24:57 -0800

    Included Navbar

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,12 +4,14 @@ export default class Footer extends Component {
   render() {
     return (
       <div className="fixed-bottom text-center">
-        <a className="navbar-brand" href="https://github.com/Rudy-Menjivar" target="_blank">
-          GitHub
-        </a>
-        <a className="navbar-brand" href="https://www.linkedin.com/in/rudy-menjivar-" target="_blank">
-          LinkedIn
-        </a>
+        <nav className="navbar-dark bg-dark">
+          <a className="navbar-brand" href="https://github.com/Rudy-Menjivar" target="_blank">
+            GitHub
+          </a>
+          <a className="navbar-brand" href="https://www.linkedin.com/in/rudy-menjivar-" target="_blank">
+            LinkedIn
+          </a>
+        </nav>
       </div>
     )
   }
