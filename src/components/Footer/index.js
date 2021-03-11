637b8bec68e600333e6104804a40e062e34671e9
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-10 17:15:12 -0800

    Footer component created with link to my github page

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="fixed-bottom text-center">
+        <a className="navbar-brand" href="https://github.com/Rudy-Menjivar" target="_blank">
+          GitHub
+        </a>
+      </div>
+    )
+  }
+}
