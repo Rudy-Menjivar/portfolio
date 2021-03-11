ce31b6dffe08cbd10a24f4395f438315c8cc896e
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-10 17:59:15 -0800

    included padding left for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Header extends Component {
   render() {
     return (
-      <div className="header">
+      <div className="header pl-4">
         <h1>Rudy Menjivar</h1>
       </div>
     )
