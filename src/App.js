79cfbbcc9b56100c0cc4752bc76cd59c428e6e66
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-10 17:42:05 -0800

    import Navbar & removed Header import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navbar />
       <Footer />
     </div>
   );
