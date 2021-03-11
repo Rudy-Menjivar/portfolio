0f8ce73dbd7ec72349be5640537c2641bdd766cc
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-11 14:21:36 -0800

    Imported Navbar and card to load them within div

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,19 +1,14 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Card from "../components/Card";
 
 function AboutMe() {
-    return (
+  return (
     <div>
-        <p>
-            Greetings! I'm currently adding to my coding skills as a student at UC Davis. I'm knowledgeable in HTML, CSS, Javascript, jQuery, Node.js, APIs, MySQL, MongoDB and React (to name a few).
-        </p>
-        <p>
-            I'll be graduating in the spring of 2021, with enough skills to work as a Full Stack Web Developer.
-        </p>
-        <p>
-            My future plans also include helping small businesses setup their online presence or at the very least, improving it. My ultimate goal is to make a positive impact with every client that I work with, building those relationships to naturally gain referrals to help more clients.
-        </p>
+      <Navbar />
+      <Card />
     </div>
-    );
-}
+  );
+};
 
 export default AboutMe;
