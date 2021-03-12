2842de872f8b04595301290a31638c9b45c1680f
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-11 21:14:57 -0800

    Included Wrapper to surround content

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,13 +1,16 @@
 import React from "react";
 import NavLinks from "../components/NavLinks";
+import Wrapper from "../components/Wrapper";
 
 function Portfolio() {
-    return (
-        <div>
-            <NavLinks />
-            <h2 className="text-center mt-5">Portfolio</h2>
-        </div>
-    )
+  return (
+    <div>
+      <NavLinks />
+      <Wrapper>
+        <h2 className="text-center mt-5">Portfolio</h2>
+      </Wrapper>
+    </div>
+  );
 };
 
 export default Portfolio;
