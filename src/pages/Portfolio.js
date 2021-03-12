5d4b8f1134910e91bcd71f5bceb5def5fde3689b
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-11 19:52:16 -0800

    Portfolio page created with heading

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavLinks from "../components/NavLinks";
+
+function Portfolio() {
+    return (
+        <div>
+            <NavLinks />
+            <h2 className="text-center mt-5">Portfolio</h2>
+        </div>
+    )
+};
+
+export default Portfolio;
