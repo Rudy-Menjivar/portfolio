3fc5d8ddaa16352d97929f9397b4af633c2a45c6
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-12 19:21:14 -0800

    data.js contains arrays of several highlighted projects

---
+++ src/components/App/data.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    name: "e-directory",
+    description: "The primary goal of e-directory is to provide quick access to an entire employee directory built with React.",
+    languages: "HTML, CSS, JavaScript, React",
+    githubLink: "https://github.com/Rudy-Menjivar/e-directory",
+    liveSite: "https://rudy-menjivar.github.io/e-directory/"
+  },
+  {
+    name: "confessions",
+    description: "Confessions is a social platform that allows users to post and share thoughts anonymously.",
+    languages: "HTML, CSS, JavaScript, React",
+    githubLink: "https://github.com/Rudy-Menjivar/confessions",
+    liveSite: ""
+  },
+  {
+    name: "budget-app",
+    description: "Budget-App is designed with offline access and functionality to reliably keep the budget on track. Expenses and deposits can be added with or without an internet connection.",
+    languages: "HTML, CSS, JavaScript,",
+    githubLink: "https://github.com/Rudy-Menjivar/budget-app",
+    liveSite: "https://dry-caverns-42087.herokuapp.com/"
+  },
+  {
+    name: "git-shopping",
+    description: "Git-Shopping is a team project involving the deployment of a fictitious E-commerce site. The site is hosted thru Heroku and uses MySQL as its database.",
+    languages: "[HTML, CSS, JavaScript, Handlebars, MySQL, passport, Heroku]",
+    githubLink: "https://github.com/Rudy-Menjivar/git-shopping",
+    liveSite: "https://gentle-cove-47687.herokuapp.com/"
+  },
+  {
+    name: "workout-tracker",
+    description: "With Workout Tracker anyone can create and track daily workouts via any web browser. Tracking includes exercise details such as type, weight, sets, reps, and duration. Additionally, cardio exercises can track total distance.",
+    languages: "[HTML, CSS, JavaScript, Express, Mongoose, Heroku]",
+    githubLink: "https://github.com/Rudy-Menjivar/workout-tracker",
+    liveSite: "https://guarded-ocean-13572.herokuapp.com/"
+  },
+  {
+    name: "readme-generator",
+    description: "This CLI application creates high-quality README files for any application. This should free the developer to devote more time to work on their project.",
+    languages: "[JavaScript]",
+    githubLink: "https://github.com/Rudy-Menjivar/readme-generator",
+    liveSite: ""
+  }
+];
