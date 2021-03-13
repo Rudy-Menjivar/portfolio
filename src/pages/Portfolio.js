ff37da7cad0eef1809d84676e676cb9ee372c7bf
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-12 18:56:32 -0800

    Importing Portfolio Card to render layout and projects for project data

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,13 +1,25 @@
 import React from "react";
 import NavLinks from "../components/NavLinks";
 import Wrapper from "../components/Wrapper";
+import PortfolioCard from "../components/PortfolioCard";
+import projects from "../components/App/data"
 
 function Portfolio() {
   return (
     <div>
       <NavLinks />
+      <h2 className="text-center mt-5">Portfolio</h2>
       <Wrapper>
-        <h2 className="text-center mt-5">Portfolio</h2>
+        {projects.map(project =>
+        <PortfolioCard
+          languages={project.languages}
+          key={project.name}
+          name={project.name}
+          description={project.description}
+          githubLink={project.githubLink}
+          liveSite={project.liveSite}
+        />
+      )}
       </Wrapper>
     </div>
   );
