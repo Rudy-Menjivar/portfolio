27fb130df6485aec8719713e46d0605982cca5b8
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-13 18:01:03 -0800

    Using Link from react-router-dom to fix issue with github pages

--- src/components/NavLinks/index.js
+++ src/components/NavLinks/index.js
@@ -1,22 +1,27 @@
 import React from "react";
-import { Nav, Navbar } from 'react-bootstrap'
+import { Link, useLocation } from "react-router-dom";
+
+const location = useLocation;
 
 function NavLinks() {
-    return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="/">Rudy Menjivar</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Link href="/portfolio">
-            Portfolio
-          </Nav.Link>
-          <Nav.Link href="/">
-            About
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-brand">Rudy Menjivar</h1>
+      <div>
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <Link to="/about" className={location.pathname === "/about" ? "nav-link" : "nav-link"}>
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/portfolio" className={location.pathname === "/portfolio" ? "nav-link" : "nav-link"}>
+              Portfolio
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
