fcefef3ebe2d43937443647c3d3843ec5cfc082c
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-30 08:44:19 -0700

    removed navlinks

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,11 +1,9 @@
 import React from "react";
-import NavLinks from "../components/NavLinks";
 import AboutCard from "../components/AboutCard";
 
 function AboutMe() {
   return (
     <div>
-      <NavLinks />
       <AboutCard />
     </div>
   );
