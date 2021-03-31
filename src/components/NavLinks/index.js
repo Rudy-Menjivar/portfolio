629ef0e4818b36f7f76da3af6c9a6d001acd4400
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-31 09:10:42 -0700

    Added fontawesome and replaced navbar span with fa icon

--- src/components/NavLinks/index.js
+++ src/components/NavLinks/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const location = useLocation;
 
@@ -9,7 +11,7 @@ function NavLinks() {
       <div className="container">
         <a className="navbar-brand" href="/">Rudy Menjivar</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+          <FontAwesomeIcon className="fawesomespan" icon={faBars} />
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
