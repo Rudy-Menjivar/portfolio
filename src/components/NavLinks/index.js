51efb0f4d240c3288d227024bfeeac13ca251e37
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-30 19:32:28 -0700

    set nav within container

--- src/components/NavLinks/index.js
+++ src/components/NavLinks/index.js
@@ -6,23 +6,25 @@ const location = useLocation;
 function NavLinks() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-      <a className="navbar-brand" href="/">Rudy Menjivar</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link to="/about" className={location.pathname === "/about" ? "nav-link active" : "nav-link"}>
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/portfolio" className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
-              Portfolio
-            </Link>
-          </li>
-        </ul>
+      <div className="container">
+        <a className="navbar-brand" href="/">Rudy Menjivar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to="/about" className={location.pathname === "/about" ? "nav-link active" : "nav-link"}>
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/portfolio" className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
+                Portfolio
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
