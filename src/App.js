8a53d9a045f407a0d2c73ffab91491ddb6af3db4
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-03-31 20:08:18 -0700

    Added contact route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavLinks from "./components/NavLinks"
 import Footer from "./components/Footer";
 import AboutMe from "./pages/AboutMe";
+import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
         <NavLinks />
         <Route exact path="/" component={AboutMe} />
         <Route exact path="/about" component={AboutMe} />
+        <Route exact path="/contact" component={Contact} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Footer />
       </div>
