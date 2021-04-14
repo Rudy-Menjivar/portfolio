da7b4e54dcb77b5fef58ac801b6cafcb70017332
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-04-13 19:00:27 -0700

    updated about me info and styling

--- src/components/AboutCard/index.js
+++ src/components/AboutCard/index.js
@@ -1,20 +1,18 @@
 import React from "react";
-import "./AboutCard.css";
+import "./StylesAbout.css";
 
 export default function AboutCard() {
   return (
-    <div className="container p5 m5">
-      <div className="wrapper p-5 m5">
-        <div className="card bg-light m-5 mx-auto">
-          <div className="card-body">
-          <h2 className="card-title">About Me</h2>
-          <p>
-            Hello! My name is Rudy and I'm a Full-Stack Web Developer. I create responsive websites that are also mobile-ready, so they are displayed correctly on computers, tablets and smartphones.
+    <div className="a-container" id="home">
+      <div className="a-section">
+        <div className="about-card">
+          <h2 className="about-title">Hi, I'm Rudy!</h2>
+          <p className="about-description">
+            I'm a Full-Stack Web Developer and I create responsive websites.
           </p>
-          <p>
-            I primarily use technologies based on MERN (MongoDB, Express, ReactJS, and NodeJs), so I can build anything from starter websites to advanced sites that require a database, or extra features. The first step involves understanding each of my client's needs and business. This not only sets up the right build but also defines the right advertising campaign with Google Adwords and any social media platform.
+          <p className="about-description">
+            I primarily use technologies based on MERN (MongoDB, Express, ReactJS, and NodeJs), so I can build anything from starter websites to advanced sites that require a database, or extra features.
           </p>
-          </div>
         </div>
       </div>
     </div>
