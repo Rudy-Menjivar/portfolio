098e6ce5a920415068177828f702ac76b95c0cd2
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-04-13 22:58:51 -0700

    Created new portfolio cards with stylesheet to also render correctly on mobile devices

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PortfolioCard from "../PortfolioCard";
+import ProjData from "../App/data";
+import "./StylesPortfolio.css";
+
+const Portfolios = () => {
+  return (
+    <div className="p-container" id="Portfolio">
+      <div className="p-cards">
+        {ProjData.map(project =>
+          <PortfolioCard
+            languages={project.languages}
+            key={project.name}
+            name={project.name}
+            description={project.description}
+            githubLink={project.githubLink}
+            liveSite={project.liveSite}
+          />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Portfolios;
