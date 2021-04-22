d5158c26398dcbe0464b7cf77cddc8f9a2b25f79
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-04-22 16:39:12 -0700

    imported images and added them to array of projects

--- src/components/App/data.js
+++ src/components/App/data.js
@@ -1,7 +1,17 @@
+import { 
+  empDir, 
+  confessions, 
+  budgetApp, 
+  gitShopping, 
+  fitTracker,
+  readmeGen 
+} from "../Images";
+
 const ProjData = [
   {
     name: "e-directory",
     description: "The primary goal of e-directory is to provide quick access to an entire employee directory built with React.",
+    image: empDir,
     languages: "HTML, CSS, JavaScript, React",
     githubLink: "https://github.com/Rudy-Menjivar/e-directory",
     liveSite: "https://rudy-menjivar.github.io/e-directory/"
@@ -9,6 +19,7 @@ const ProjData = [
   {
     name: "confessions",
     description: "Confessions is a social platform that allows users to post and share thoughts anonymously.",
+    image: confessions,
     languages: "HTML, CSS, JavaScript, React",
     githubLink: "https://github.com/Rudy-Menjivar/confessions",
     liveSite: "https://app-confessions.herokuapp.com/"
@@ -16,6 +27,7 @@ const ProjData = [
   {
     name: "budget-app",
     description: "Budget-App is designed with offline access and functionality to reliably keep the budget on track. Expenses and deposits can be added with or without an internet connection.",
+    image: budgetApp,
     languages: "HTML, CSS, JavaScript,",
     githubLink: "https://github.com/Rudy-Menjivar/budget-app",
     liveSite: "https://dry-caverns-42087.herokuapp.com/"
@@ -23,6 +35,7 @@ const ProjData = [
   {
     name: "git-shopping",
     description: "Git-Shopping is a team project involving the deployment of a fictitious E-commerce site. The site is hosted thru Heroku and uses MySQL as its database.",
+    image: gitShopping,
     languages: "HTML, CSS, JavaScript, Handlebars, MySQL, passport, Heroku",
     githubLink: "https://github.com/Rudy-Menjivar/git-shopping",
     liveSite: "https://gentle-cove-47687.herokuapp.com/"
@@ -30,6 +43,7 @@ const ProjData = [
   {
     name: "workout-tracker",
     description: "With Workout Tracker anyone can create and track daily workouts via any web browser. Tracking includes exercise details such as type, weight, sets, reps, and duration. Additionally, cardio exercises can track total distance.",
+    image: fitTracker,
     languages: "HTML, CSS, JavaScript, Express, Mongoose, Heroku",
     githubLink: "https://github.com/Rudy-Menjivar/workout-tracker",
     liveSite: "https://guarded-ocean-13572.herokuapp.com/"
@@ -37,6 +51,7 @@ const ProjData = [
   {
     name: "readme-generator",
     description: "This CLI application creates high-quality README files for any application. This should free the developer to devote more time to work on their project.",
+    image: readmeGen,
     languages: "JavaScript",
     githubLink: "https://github.com/Rudy-Menjivar/readme-generator",
     liveSite: "https://github.com/Rudy-Menjivar/readme-generator"
