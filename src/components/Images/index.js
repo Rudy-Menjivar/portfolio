c275c19c1c0c28942a08958fc3fe30e97c8b796f
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-04-22 16:37:19 -0700

    including project images

---
+++ src/components/Images/index.js
@@ -0,0 +1,6 @@
+export { default as empDir } from "./e-directory.png";
+export { default as budgetApp } from "./budget-app.png";
+export { default as confessions } from "./confessions-img.png";
+export { default as gitShopping } from "./git-shopping.png";
+export { default as fitTracker } from "./fitness-tracker.png";
+export { default as readmeGen } from "./readme-generator.png";
