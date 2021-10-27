323681efe2b5ddee9bc5a0f0935bbcfe2fb66ac2
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-10-26 20:15:56 -0700

    updated styling for external footer link

--- src/components/Footer/StylesFooter.js
+++ src/components/Footer/StylesFooter.js
@@ -55,7 +55,7 @@ export const FooterLinkTitle = styled.h1`
   margin-bottom: 16px;
 `;
 
-export const FooterLink = styled(Link)`
+export const FooterLink = styled.a`
   color: rgb(250, 250, 250);
   text-decoration: none;
   margin-bottom: 0.5rem;
